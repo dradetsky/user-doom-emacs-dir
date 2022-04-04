d23488c8de6fb392df5dcdecbffa7c5222fe0260
Daniel Radetsky <dradetsky@gmail.com>
2022-04-04 12:27:13 -0700

    fonts

--- autoload/font.el
+++ autoload/font.el
@@ -3,3 +3,9 @@
   "Return non-nil if FONT exists on this system."
   (declare (pure t) (side-effect-free t))
   (ignore-errors (find-font (doom-normalize-font font))))
+
+(defun doom-font-exists-with-warning-p (font)
+  (let ((foundp (doom-font-exists-p font)))
+    (unless foundp
+      (message (format "couldnt find font %s" font)))
+    foundp))
