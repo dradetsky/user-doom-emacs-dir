83c5d5ef677709ae481f65c34a14cf69fc803f43
Daniel Radetsky <dradetsky@gmail.com>
2024-09-28 15:54:36 -0700

    mail

--- modules/email/mutt/config.el
+++ modules/email/mutt/config.el
@@ -2,3 +2,8 @@
 
 (add-to-list 'auto-mode-alist
              '("/tmp/neomutt-" . mail-mode))
+
+(defun dmr:mail-mode-hook ()
+  (setq fill-column 60))
+
+(add-hook! 'mail-mode-hook #'dmr:mail-mode-hook)
