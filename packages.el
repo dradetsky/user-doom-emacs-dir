59e59937ae797986d8f84b7deb92f7ec94230430
Daniel Radetsky <dradetsky@gmail.com>
2024-10-28 17:00:26 -0700

    pkgs

--- packages.el
+++ packages.el
@@ -17,9 +17,11 @@
 
 (package! vline :pin "f5d7b5743dceca75b81c8c95287cd5b0341debf9")
 
+(package! disable-mouse :pin "93a55a6453f34049375f97d3cf817b4e6db46f25")
 
-;; Use `:pin' to specify a particular commit to install.
-;(package! builtin-package :pin "1a2b3c4d5e")
+;; AFAICT this is a big slow python thing suitable for big python projects, but
+;; mostly just slows me down when I touch a big python project occasionally
+(package! anaconda-mode :disable t)
 
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
